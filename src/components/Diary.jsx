e866b9127aca4d07d6593698459a9f80bf9e934f
eunseo <eundeok9@gmail.com>
2023-04-25 00:14:48 +0900

    추가 과제 답안 작성

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -12,7 +12,7 @@ const Diary = () => {
     setContent(e.target.value);
   };
 
-  const handleReset = (e) => {
+  const handleReset = () => {
     setSubject("");
     setContent("");
   };
@@ -23,11 +23,44 @@ const Diary = () => {
     setContent("");
   };
 
+  // useState 한 줄만 사용시
+
+  // const [data, setData] = useState({
+  //   subject: "",
+  //   content: "",
+  // });
+
+  // const handleData = (e) => {
+  //   setData({
+  //     ...data,
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
+
+  // const handleReset = () => {
+  //   setData({
+  //     subject: "",
+  //     content: "",
+  //   });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   alert(`제목: ${data.subject} \n내용: ${data.content} `);
+  //   setData({
+  //     subject: "",
+  //     content: "",
+  //   });
+  // };
+
   return (
     <div className="wrapper">
-      <input value={subject} placeholder="제목을 입력해주세요" onChange={handleSubject} />
+      <input name="subject" value={subject} placeholder="제목을 입력해주세요" onChange={handleSubject} />
       <br />
-      <textarea value={content} placeholder="내용을 입력해주세요" onChange={handleContent} />
+      <textarea name="content" value={content} placeholder="내용을 입력해주세요" onChange={handleContent} />
+      {/* useState 한 줄만 사용 시 */}
+      {/* <input name="subject" value={data.subject} placeholder="제목을 입력해주세요" onChange={handleData} />
+      <br />
+      <textarea name="content" value={data.content} placeholder="내용을 입력해주세요" onChange={handleData} /> */}
       <br />
       <button onClick={handleReset}>초기화</button>
       <br />
